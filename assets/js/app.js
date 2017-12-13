6a04f2157d16a7480a455530ddda00a1933ff098
AGuerrav <aaguerravaldes@gmail.com>
2017-12-13 18:14:19 -0300

    filtro funcional

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
-    setTimeout(function() {
-        $(".fill").fadeOut(0);
+  //para que desaparezca la primera pantalla
+  setTimeout(function() {
+    $(".fill").fadeOut(0);
     },5000);
+  //para agregar la clase active que utilizare para mostrar el filtro activo
+  $('.category-list .categoryItem[category="all"]').addClass('active');
+  //funcion para cuando haga click en cada filtro "filtre"
+  $('.categoryItem').click(function() {
+
+    var categoryProduct = $(this).attr('category'); //almacenar en la variable la categoria cuando haga click
+    //console.log(categoryProduct);
+    //poner clase active al filtro al que le hago click
+    $('.categoryItem').removeClass('active');
+    $(this).addClass('active');
+    //cuando haga click ocultar todos los productos
+    $('.product-item').hide();
+    //y que muestre todos los que correspondan a la categoria
+    $('.product-item[category="'+ categoryProduct+'"]').show();
+  })
+  //funcion para que se muestre todo cuando haga click en el filtro todos los restaurantes
+  $('.categoryItem[category="all"]').click(function(){
+    $('.product-item').show();
+
+  })
+
+
+
+
+
+
+
 });
